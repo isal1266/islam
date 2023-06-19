f54dec28e1891a703989ad4a5dc4b0207909b48d
isal1266 <128419325+isal1266@users.noreply.github.com>
2023-06-19 17:18:11 -0300

    2 files modified

--- sripts.js
+++ sripts.js
@@ -1,6 +1,16 @@
-function dosomethingcool() {
+function doSomethingCool() {
 // alert('Warning!');
 
-var x = 'warning (but in a var)';
-alert(x);
+// var x = 'warning (but in a var)';
+// var x = Math.rondom() * 10;
+// var y = math. floor(x);
+// alert(y);
+// 
+// var name = document.querySelector('#name').value;
+
+var div = document.querySelector(".child");
+var p = document.creatElement('p');
+p.textContent = 'I was appended to the div';
+div.appendChild(p);
+
 }
